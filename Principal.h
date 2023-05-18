f2b2ec4428fb6cbdc4043d6c9b47f8ef3d4ed641
ctecchiog <127995834+ctecchiog@users.noreply.github.com>
2023-05-18 14:39:09 -0300

    Update Principal.h

--- Principal.h
+++ Principal.h
@@ -1,11 +1,7 @@
 #pragma once
 #include "stdafx.h"
-<<<<<<< HEAD
-#include <SFML/Graphics.hpp>
-=======
 #include "Jogador.h"
 #include <SFML\Graphics.hpp>
->>>>>>> 15d3c64a143286dba7eb4417abbf3f4e8e99bce9
 
 class Principal {
 private:
