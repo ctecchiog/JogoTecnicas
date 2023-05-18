706d7c383df6ebd725f740f4ff7b7dbde25003d6
ctecchiog <127995834+ctecchiog@users.noreply.github.com>
2023-05-18 14:36:08 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include "stdafx.h"
-<<<<<<< HEAD
-#include <SFML/Graphics.hpp>
-=======
 #include "Principal.h"
 #include <SFML\Graphics.hpp>
->>>>>>> 15d3c64a143286dba7eb4417abbf3f4e8e99bce9
 
 int main()
 {
